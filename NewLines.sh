ece6c53c589714a470673eec606de2bd876d7f56
Tyler Kuipers <Tyler.Kuipers@ama.ab.ca>
2020-08-14 14:13:56 -0600

    Fix NewLines Script

--- NewLines.sh
+++ NewLines.sh
@@ -1,13 +1,10 @@
 #!/bin/sh; !C:/Program\ Files/Git/usr/bin/sh.exe
-# Usage:
-# Remove the .sh file extension when you put the script in your hooks folder!
 
 files=$(git diff-index --name-status --cached HEAD | grep -v ^D | cut -c3-)
 if [ "$files" != "" ]
 then
   for f in $files
   do
-    # Only examine known text files
     if [[ "$f" =~ [.](conf|css|erb|html|js|json|log|properties|rb|ru|txt|xml|yml|h|m|gs|gsx|sh)$ ]]
     then
       # Add a linebreak to the file if it doesn't have one
@@ -24,7 +21,9 @@ then
         git add $f
       fi
 
-      cat -s $f > $f
+      #Remove multiple blank lines
+      sed -i "" -e '/^$/N;/^\n$/D' $f
+      git add $f
     fi
   done
 fi
